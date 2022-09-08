a77a9cdebb8934c3f02d153a313801daf6c2237c
Lars Talian <113114512+larstalian@users.noreply.github.com>
2022-09-08 15:24:08 +0200

    Delete barelek directory

--- barelek/src/oppgave2.java
+++
@@ -1,9 +0,0 @@
-public class oppgave2 {
-    public static void main(String[] args) {
-        double meatA = 35.90, meatB = 35.90;
-        int weightA = 450, weightB = 500;
-        double cost_per_gramA = (meatA / weightA), cost_per_gramB = (meatB / weightB);
-        boolean b = cost_per_gramA < cost_per_gramB;
-        System.out.println("KjøttA er billigere enn kjøtt B: " + b);
-    }
-}
