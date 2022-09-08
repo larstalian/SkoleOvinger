ad9c232daac250fc544a105afbb928436b47b122
ltst <l.t.stangebyehansen@gmail.com>
2022-09-08 15:30:31 +0200

    1

--- barelek/src/Oppgave1.java
+++ barelek/src/Oppgave1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Oppgave1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean leapYear = false;
