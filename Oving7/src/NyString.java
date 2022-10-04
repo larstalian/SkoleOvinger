391eab5c0f857898c36f4ea1d8da1aca418dcc48
ltst <l.t.stangebyehansen@gmail.com>
2022-10-04 18:36:28 +0200

    Øving 7

--- Oving7/src/NyString.java
+++ Oving7/src/NyString.java
@@ -1,52 +1,27 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class NyString {
-//    Klassen String inneholder mange metoder. Vi skal nå lage noen flere.
-//    Lag en klasse som heter NyString. Den skal ha en objektvariabel av klassen String. Klassen NyString skal være immutabel og tilby følgende tjenester:
-//            • Konstruktør som tar et objekt av klassen String som argument.
-//            • Forkorting. Forkortingen skal skje ved at første tegn i hvert ord plukkes ut.
-//              Bruk mellomrom som skille mellom ordene. Eksempel: «denne setningen kan forkortes» skal forkortes til «dskf».
-//              Tips: Bruk String-metoden split(), som returnerer en tabell med ordene som teksten består av: String() ord = tekst.split()
-//            • Fjerning av tegn. Eksempel: Dersom tegnet ‘e’ fjernes fra teksten «denne setningen kan forkortes», står vi igjen
-//            med «dnn stningn kan forkorts». Tips: Bruk en while-løkke og for eksempel String-metodene indexOf() og substring().
+    private final String text;
 
-    private final String tekst;
-
-    public NyString(String tekst) {
-        this.tekst = tekst;
+    public NyString(String text) {
+        this.text = text;
     }
 
-    public String[] shortening() {
-        String[] result = new String[tekst.length()];
-        for (int i = 0; i < tekst.length(); i++) {
-            String letter = String.valueOf(tekst.charAt(i));
-            int[] shortened;
-            if (letter.equals(" ")) {
-                result[i] = String.valueOf(tekst.charAt(i + 1));
-            }
-        }
-        return result;
+    public String getText() {
+        return text;
     }
 
-    public String removeSign() {
-
-        return null;
+    public String[] getInitials() {
+        String[] arrayNoWhitespace = getText().split(" ");
+        return Arrays.stream(arrayNoWhitespace).map(s -> s.substring(0, 1)).toArray(String[]::new);
     }
 
-    public void printText(String[] result) {
-        for (String s : result) {
-            System.out.println(s);
-        }
+    public void printArray(String[] array) {
+        Stream.of(array).forEach(System.out::print);
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String tekst = in.next();
-        NyString object = new NyString(tekst);
-        object.printText(object.shortening());
-
-
+    public String[] removeCharacter(String inputRemoveCharacter) {
+        return getText().split(inputRemoveCharacter);
     }
-
 }
---
+++ Oving7/src/NyStringClient.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NyStringClient {
+    private static void client() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\nThis program returns a text's initials and removes a chosen character from the text\nPlease enter text:");
+        String textInput = in.nextLine();
+
+        NyString text = new NyString(textInput);
+        text.printArray(text.getInitials());
+
+        System.out.println("\n\nEnter the character to remove ");
+        String inputRemoveCharacter = in.next();
+
+        System.out.println("Text with " + inputRemoveCharacter + " removed:");
+        text.printArray(text.removeCharacter(inputRemoveCharacter));
+    }
+
+    public static void main(String[] args) {
+        client();
+    }
+}
