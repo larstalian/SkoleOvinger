f81bc72e3767f62a3f2ff60900af2c4696e135ea
Lars Talian <113114512+larstalian@users.noreply.github.com>
2022-10-09 21:00:11 +0200

    Delete learner/src directory

--- learner/src/CompositionTest.java
+++
@@ -1,8 +0,0 @@
-public class CompositionTest {
-    public static void main(String[] args) {
-        CompositionBirthday birthdayObject = new CompositionBirthday(3, 4, 5);
-        CompositionName nameObject = new CompositionName("Talian",birthdayObject);
-
-        System.out.println(nameObject);
-    }
-}
