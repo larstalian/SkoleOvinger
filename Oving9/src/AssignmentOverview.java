064309889b04ed43ab15d387aeef75b06ff925e3
ltst <l.t.stangebyehansen@gmail.com>
2022-10-09 17:42:29 +0200

    Øving 9 ferdig :)

--- Oving9/src/AssignmentOverview.java
+++ Oving9/src/AssignmentOverview.java
@@ -1,48 +1,193 @@
 import java.util.ArrayList;
-import java.util.List;
-//Les på aggregering, komposisjon og dyp kopi
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * The AssignmentOverview class.
+ */
 public class AssignmentOverview {
-    private List<Student> students = new ArrayList<>();
+
+    private final ArrayList<Student> students;
+
     private int numberOfStudents = 0;
 
-
+    /**
+     * The AssignmentOverview constructor.
+     * Creates a new arraylist students.
+     */
     public AssignmentOverview() {
-        students = new ArrayList<Student>();
+        students = new ArrayList<>();
     }
 
-    public List<Student> getStudents() {
-        return students;
+    /**
+     * The main method.
+     *
+     * @param args main.
+     */
+    public static void main(String[] args) {
+        client();
     }
 
-    public int getNumberOfStudents() {
-        return students.size();
-    }
+    /**
+     * The client method.
+     */
+    public static void client() {
+        Scanner in = new Scanner(System.in);
+        Scanner inString = new Scanner(System.in);
+        AssignmentOverview main = new AssignmentOverview();
 
-    public void newStudent(String name, int numberOfApprovedAssignments) {
-        boolean nameExists = false;
-        for (int i = 0; i < getNumberOfStudents(); i++) {
-            if (students.get(i).getName().equals(name)) {
-                nameExists = true;
+        System.out.println("This program lets you register new students "
+                + "and track their approved assignments");
+
+        boolean run = true;
+
+        while (run) {
+            System.out.println("""
+                                        
+                    Make a choice:
+                    1. Register a new student
+                    2. Find a students passed assignments
+                    3. Increase a students number of passed assignments
+                    4. Get list of all registered students
+                    5. Exit
+                     """);
+
+            int switchChoice = in.nextInt();
+            switch (switchChoice) {
+                case 1 -> {
+                    System.out.println("\nPlease enter the new students name, "
+                            + "then their current number of approved assignments");
+                    String name = inString.nextLine();
+                    int numberOfApprovedAssignments = in.nextInt();
+                    main.registerNewStudent(name, numberOfApprovedAssignments);
+                }
+                case 2 -> {
+                    System.out.println("\nEnter the students name:");
+                    String name = inString.nextLine();
+                    try {
+                        System.out.println("\nThe student has "
+                                + main.getStudentFromArray(name).getNumberOfApprovedAssignments()
+                                + " approved assignments");
+
+                    } catch (Exception e) {
+                        System.out.println("\nThe student does not exist");
+                    }
+                }
+                case 3 -> {
+                    System.out.println("\nEnter the students name: ");
+                    String name = inString.nextLine();
+                    System.out.println("\nEnter the increase in number of approved assignments: ");
+                    int increase = in.nextInt();
+                    try {
+                        main.getStudentFromArray(name).increaseApprovedAssignments(increase);
+                        System.out.println("\nThe student " + name + " now has "
+                                + main.getStudentFromArray(name).getNumberOfApprovedAssignments()
+                                + " approved assignments");
+
+                    } catch (Exception e) {
+                        System.out.println("\nThe student does not exist");
+                    }
+                }
+                case 4 -> System.out.println("\nThe registered students are: " + main);
+                case 5 -> {
+                    System.out.println("The program exits");
+                    run = false;
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + switchChoice);
             }
         }
-        if (!nameExists) {
-            Student newStudent = new Student(name, numberOfApprovedAssignments);
-            students.add(newStudent);
+
+    }
+
+    /**
+     * A toString method which returns a list of Students registered.
+     *
+     * @return the student list
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < getNumberOfStudents(); i++) {
+            s.append(students.get(i).getName()).append(". Approved assignments: ")
+                    .append(students.get(i).getNumberOfApprovedAssignments()).append(" | ");
+        }
+        return s.toString();
+    }
+
+    /**
+     * Gets the student from the students array list.
+     *
+     * @param studentName The students name
+     * @return the student object that matches the student name input
+     */
+    public Student getStudentFromArray(String studentName) {
+        if (findStudent(studentName) == -1) {
+            return null;
+        }
+        return students.get(findStudent(studentName));
+    }
+
+    /**
+     * Get method for finding the number of students.
+     *
+     * @return the number of students.
+     */
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    /**
+     * Method for registering a new student.
+     *
+     * @param name                        The students name
+     * @param numberOfApprovedAssignments The number of approved assignments
+     */
+    public void registerNewStudent(String name, int numberOfApprovedAssignments) {
+        if (studentExists(name)) {
+            students.add(new Student(name, numberOfApprovedAssignments));
+            numberOfStudents += 1;
+            System.out.println("The student " + name + " has been created. "
+                    + "Their current number of approved assignments are "
+                    + numberOfApprovedAssignments + ".");
 
         } else {
-            System.out.println("The student already exists");
+            System.out.println("Student already exists");
         }
-
     }
-    public String toString() {
-        StringBuilder text = new StringBuilder();
+
+    /**
+     * A help method to check if a student already exists.
+     *
+     * @param name The students name
+     * @return true if the student already exists
+     */
+    public boolean studentExists(String name) {
+        boolean b = true;
+        for (Student value : students) {
+            if (Objects.equals(value.getName(), name)) {
+                b = false;
+                break;
+            }
+        }
+        return b;
+    }
+
+    /**
+     * A help method to find the student object
+     * in the student array list using the input String for name.
+     *
+     * @param name The input String for name
+     * @return the position of the Student object that matches the input String
+     */
+
+    public int findStudent(String name) {
+        int index = -1;
         for (int i = 0; i < getNumberOfStudents(); i++) {
-            text.append(students.get(i).toString()).append(" | ");
+            if (students.get(i).getName().equals(name)) {
+                index = i;
+                break;
+            }
         }
-        return text.toString();
+        return index;
     }
-
-
-
-
 }
--- Oving9/src/Student.java
+++ Oving9/src/Student.java
@@ -1,24 +1,45 @@
-public class Student {
-    public Student(String name, int numberOfApprovedAssignments){
-        this.name = name;
-        this.numberOfApprovedAssignments = numberOfApprovedAssignments;
-    }
+/**
+ * The student class.
+ */
+public final class Student {
     private final String name;
     private int numberOfApprovedAssignments;
 
+    /**
+     * The student class constructor.
+     *
+     * @param name                        The name of the student
+     * @param numberOfApprovedAssignments The number of approved assignments
+     */
+    public Student(String name, int numberOfApprovedAssignments) {
+        this.name = name;
+        this.numberOfApprovedAssignments = numberOfApprovedAssignments;
+    }
+
+    /**
+     * Get student name method.
+     *
+     * @return Returns this students name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the number of approved assignments.
+     *
+     * @return Returns the number of approved assignments
+     */
     public int getNumberOfApprovedAssignments() {
         return numberOfApprovedAssignments;
     }
 
-    public void increaseNumberOfApprovedAssignments(int numberOfApprovedAssignments) {
-        this.numberOfApprovedAssignments = numberOfApprovedAssignments + 1;
-    }
-
-    public String toString() {
-        return "ee";
+    /**
+     * Increases the number of approved assignments for this student.
+     *
+     * @param increase The increase of approved assignments
+     */
+    public void increaseApprovedAssignments(int increase) {
+        this.numberOfApprovedAssignments += increase;
     }
 }
