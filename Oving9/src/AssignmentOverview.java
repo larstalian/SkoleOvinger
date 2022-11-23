98f4f68499fceb951ab1eb178cfe9f2dd3ec6a94
Lars Talian <l.t.stangebyehansen@gmail.com>
2022-11-23 20:01:44 +0100

    update

---
+++ Oving9/src/AssignmentOverview.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+/**
+ * The AssignmentOverview class. Contains the client, list of students and methods to register
+ * students and view/edit their approved assignments
+ */
+public class AssignmentOverview {
+
+  private final ArrayList<Student> students;
+
+  private int numberOfStudents = 0;
+
+  /** The AssignmentOverview constructor. Creates a new arraylist students. */
+  public AssignmentOverview() {
+    students = new ArrayList<>();
+  }
+
+  /**
+   * Get-method for this student from the students array list.
+   *
+   * @param studentName This students name
+   * @return the student object that matches the student name input
+   */
+  public Student findStudentWithName(String studentName) {
+    return students.stream()
+        .filter(student -> student.getName().equalsIgnoreCase(studentName))
+        .findFirst()
+        .orElse(null);
+  }
+
+  /**
+   * Get-method for finding the number of students.
+   *
+   * @return the number of students.
+   */
+  public int getNumberOfStudents() {
+    return numberOfStudents;
+  }
+
+  /**
+   * Method for registering a new student. Uses the studentExists help method.
+   *
+   * @param name This students name
+   * @param numberOfApprovedAssignments The number of approved assignments
+   */
+  public boolean registerNewStudent(String name, int numberOfApprovedAssignments) {
+    if (!studentExists(name)) {
+      students.add(new Student(name, numberOfApprovedAssignments));
+      numberOfStudents += 1;
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Method to check if a student already exists. Used by the registerStudent method.
+   *
+   * @param name This students name
+   * @return true if the student already exists
+   */
+  public boolean studentExists(String name) {
+    return findStudentWithName(name) != null;
+  }
+
+  /**
+   * Method to create a string of the 'students' array.
+   *
+   * @return the string
+   */
+  public String arrayBuilder() {
+    StringBuilder s = new StringBuilder();
+    students.forEach(s::append);
+    return String.valueOf(s);
+  }
+}
